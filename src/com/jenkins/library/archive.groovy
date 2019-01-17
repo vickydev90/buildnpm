9183da4c700c65835027118d941fbcdf72517a5e
Vishal Deshmukh <vishal.deshmukh@sapient.com>
2019-01-17 15:59:14 +0000

    archive

--- src/com/jenkins/library/archive.groovy
+++ src/com/jenkins/library/archive.groovy
@@ -5,19 +5,19 @@ def runfunction() {
 
 	String configPath = "${env.WORKSPACE}/pipelines/conf/build-nodejs.yaml"
 	Map configFile = readYaml file: configPath
-	
+	def targetEnv = "${env.branch}"
 	def nexusURL = configFile.nexus.url ? configFile.nexus.url.toString() : "null"
 	def credentialsID = configFile.nexus.credentials ? configFile.nexus.credentials.toString() : "null"
 	def appName = configFile.application ? configFile.application.toString() : "null"
 	def packageVersion = configFile.packageVersion ? configFile.packageVersion.toString() : "null"
-	String artifact = "${appName}-artifact-${packageVersion}.tar.gz"
+	String artifact = "${appName}-${targetEnv}-artifact-${packageVersion}.tar.gz"
 	
 	  writeFile file: '/tmp/package.sh', text: libraryResource('package.sh')
 	  def pack = "chmod +x /tmp/package.sh"
 	  sh(returnStdout: true, script: pack)
 	  sh """/tmp/package.sh ${artifact}"""
 	  dir('j2') {
-      stash name: "artifact-${context.application}", includes: artifact
+      stash name: "artifact-${context.application}-${targetEnv}", includes: artifact
       archiveArtifacts 	artifacts: artifact, onlyIfSuccessful: true
     }
     }
