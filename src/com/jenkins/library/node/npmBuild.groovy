25ded12f945b855becaf7204a448b094470481a4
Vishal Deshmukh <vishal.deshmukh@sapient.com>
2019-01-17 12:39:19 +0000

    changed exception

--- src/com/jenkins/library/node/npmBuild.groovy
+++ src/com/jenkins/library/node/npmBuild.groovy
@@ -1,23 +1,25 @@
 package com.jenkins.library.node
 import com.jenkins.library.execution.CommandExecutor
 
-def npmRun(Map configFile) {
+def npmRun(Map configFile, String runexe) {
 	println "Initialting build"
 
-	def preparedCommand = prepareCommand(configFile)
+	def preparedCommand = prepareCommand(configFile, runexe)
 	new CommandExecutor().execute(preparedCommand)
-	
-//	def NpmRun = NpmRun(configFile)
-//	new CommandExecutor().execute(NpmRun)
 }
 
-def prepareCommand(Map configFile){
+def prepareCommand(Map configFile, String runexe){
 	try{
 		// Default build goals are 'install'
-		def buildGoals = configFile.node.npm_goals ? configFile.node.npm_goals.toString() : "install"
+		if ( runexe == 'npm' ) {
+			def buildGoals = configFile.node.npm_goals ? configFile.node.npm_goals.toString() : "install"
+		}
+		else if ( runexe == 'npm run' )  {
+		    def buildGoals = configFile.node.npmRun_goals ? configFile.node.npmRun_goals.toString() : "build"                              
+		                              }
 
 		println "Creating Command for execution ...."
-		def command = "npm"
+		def command = runexe
 		def space = " "
 
 		// append build goals
@@ -32,25 +34,3 @@ def prepareCommand(Map configFile){
 		throw ex
 	}
 }
-
-/* def NpmRun(Map configFile){
-	try{
-		// Default build goals are 'clean package'
-		def buildGoals = configFile.node.npmRun_goals ? configFile.node.npmRun_goals.toString() : "release"
-
-		println "Creating Command for execution ...."
-		def command = "npm run"
-		def space = " "
-
-		// append build goals
-		command = command + space + buildGoals
-		println "command is : " + command
-		return command
-	} catch (groovy.lang.MissingMethodException error) {
-		println "Exception Expected "
-		throw error
-	} catch (Exception ex) {
-		println "FAILURE: runNpm(): ${ex.message}"
-		throw ex
-	}
-} */
